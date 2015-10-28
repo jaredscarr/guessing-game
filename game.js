82bbf1d477f7bc8e97655c77464c8111bf1896d5
JARED SCARR <JAREDSCARR@GMAIL.COM>
2015-10-28 13:25:52 -0700

    put while loops around all questions. Question four is not working. Must fix. The rest works.

--- game.js
+++ game.js
@@ -1,18 +1,12 @@
 var correct = 0;
-var answer1, answer2, answer3, answer4;
-var question1, question2, question3;
-var years = 0;
-
 var name = prompt('Hello, what is your name?');
 alert('Hello, ' + name + '.' + '\nPlease answer the following questions with a yes or no.');
 
-//this is where you left off! playing with a do while loop. It works!
-// add with resetting the i to 0. put do {} while around the entire question for 2, 3 and 4
 var i = 0;
-do {
+while (i < 1) {
 
-  question1 = prompt('Does Jared like camping?');
-  answer1 = 'yes';
+  var question1 = prompt('Does Jared like camping?');
+  var answer1 = 'yes';
   question1 = question1.toLowerCase();
 
   if (question1 === 'yes' || question1 === 'y') { // this process should be a function
@@ -25,14 +19,9 @@ do {
     alert("I'm sorry " + name + " that is not a valid input.");
     i = 0;
   }
+}
 
-} while (i < 1);
-
-// end while loop. also perhaps the end of the function
-
-//below code all works but no loops have been implemented.
-
-/*if (question1 === answer1) {
+if (question1 === answer1) {
   alert("That's right, " + name + '!');
   console.log ('The user guessed correctly.');
   correct += 1;
@@ -41,17 +30,22 @@ do {
   console.log('the user guessed incorrectly.');
 }
 
+i = 0;
+while (i < 1) {
 
-question2 = prompt('Does Jared have any pets?');
-answer2 = 'no';
-question2 = question2.toLowerCase();
+  var question2 = prompt('Does Jared have any pets?');
+  var answer2 = 'no';
+  question2 = question2.toLowerCase();
 
-if (question2 === 'yes' || question2 === 'y') {
-  question2 = 'yes';
-} else if (question2 === 'no' || question2 === 'n') {
-  question2 = 'no';
-} else {
-    alert("I'm sorry " + name + " that is not a valid input.");
+  if (question2 === 'yes' || question2 === 'y') {
+    question2 = 'yes';
+    i = 2;
+  } else if (question2 === 'no' || question2 === 'n') {
+    question2 = 'no';
+    i = 2;
+  } else {
+      alert("I'm sorry " + name + " that is not a valid input.");
+  }
 }
 
 if (question2 === answer2) {
@@ -63,16 +57,21 @@ if (question2 === answer2) {
     console.log('the user guessed incorrectly.');
     }
 
-question3 = prompt('Does Jared enjoy cooking?');
-answer3 = 'yes';
-question3 = question3.toLowerCase();
+i = 0;
+while (i < 1) {
+  var question3 = prompt('Does Jared enjoy cooking?');
+  var answer3 = 'yes';
+  question3 = question3.toLowerCase();
 
-if (question3 === 'yes' || question3 === 'y') {
-  question3 = 'yes';
-} else if (question3 === 'no' || question3 === 'n') {
-  question3 = 'no';
-} else {
-    alert("I'm sorry " + name + " that is not a valid input.");
+  if (question3 === 'yes' || question3 === 'y') {
+    question3 = 'yes';
+    i = 2;
+  } else if (question3 === 'no' || question3 === 'n') {
+    question3 = 'no';
+    i = 2;
+  } else {
+      alert("I'm sorry " + name + " that is not a valid input.");
+  }
 }
 
 if (question3 === answer3) {
@@ -84,13 +83,16 @@ if (question3 === answer3) {
     console.log('the user guessed incorrectly.');
 }
 
-answer4 = 12;
+var answer4 = 12;
+var years = '';
 
-years = parseInt(prompt('How many years did Jared spend in the hospitality industry?'));
-
-// if (years === NaN) {
-//   alert("I'm sorry that is not valid input.");
-// }
+// the below is broken. Fix this question and loop.
+while (years === NaN) {
+  years = parseInt(prompt('How many years did Jared spend in the hospitality industry?'));
+  if (years === NaN) {
+   alert("I'm sorry that is not valid input.");
+ }
+}
 
 if (years === answer4) {
   alert('That is right ' + name + '! Way to go!');
@@ -102,9 +104,9 @@ if (years === answer4) {
 }
 
 
-alert('For question 1 you guessed ' + question1 + '. The correct answer was ' + answer1 + '.');
-alert('For question 2 you guessed ' + question2 + '. The correct answer was ' + answer2 + '.');
-alert('For question 3 you guessed ' + question3 + '. The correct answer was ' + answer3 + '.');
-alert('For question 4 you guessed ' + years + '. The correct answer was ' + answer4 + '.');
+// alert('For question 1 you guessed ' + question1 + '. The correct answer was ' + answer1 + '.');
+// alert('For question 2 you guessed ' + question2 + '. The correct answer was ' + answer2 + '.');
+// alert('For question 3 you guessed ' + question3 + '. The correct answer was ' + answer3 + '.');
+// alert('For question 4 you guessed ' + years + '. The correct answer was ' + answer4 + '.');
 
-alert('You got ' + correct + ' answers correct.');*/
+// alert('You got ' + correct + ' answers correct.');
